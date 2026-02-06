f795a58f94b6df5d988f7799c5f2998173f8a2f5
vardhanyahoo999-ship-it <vardhan.yahoo999@gmail.com>
2026-02-06 10:34:53 +0530

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,12 @@
+const videos = [
+  {
+    title: "Navya Function",
+    thumbnail: "thumbnails/video1.jpg",
+    driveLink: "https://drive.google.com/drive/folders/1ddRx59ZSbD5_w5w0QG35_k-uC5r1lNKK"
+  },
+  {
+    title: "Birthday Function",
+    thumbnail: "thumbnails/video2.jpg",
+    driveLink: "https://drive.google.com/file/d/FILE_ID_2/view"
+  }
+];
